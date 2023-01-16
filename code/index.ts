227777d413d54f4e0dbc11952aa5e771df4c7b8f
Yone Shinzato <shinzato.yone@capco.com>
2023-01-16 16:47:00 -0300

    literal type

--- code/index.ts
+++ code/index.ts
@@ -97,19 +97,29 @@
 // console.log(user);
 
 //union types pode armazenar dois tipos
-function userInput(input1: number | string, input2: number | string) {
-    let result
-    if(typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2
-    } else {
-        result = input1.toString() + ' ' +  input2.toString()
-    }
-    return result
-}
+// function userInput(input1: number | string, input2: number | string) {
+//     let result
+//     if(typeof input1 === 'number' && typeof input2 === 'number') {
+//         result = input1 + input2
+//     } else {
+//         result = input1.toString() + ' ' +  input2.toString()
+//     }
+//     return result
+// }
 
-const combinePrices = userInput(10, 20);
-console.log(combinePrices);
+// const combinePrices = userInput(10, 20);
+// console.log(combinePrices);
 
-const combineName = userInput('Apple', 'Avaocado');
-console.log(combineName);
+// const combineName = userInput('Apple', 'Avaocado');
+// console.log(combineName);
+
+//literal type 
+// let productPrice: 30 | 20 | 10
+// productPrice = 30
+
+// let productName: 'Banana' | 'Apple' | 'Pear'
+// productName = 'Pear'
+// productName = 'Banana'
+
+
 
