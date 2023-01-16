1e93f4ff7f4353102a6059e1a5eefa3ed7dede54
Yone Shinzato <shinzato.yone@capco.com>
2023-01-16 18:02:52 -0300

    exercícios de 1 a 3 done

---
+++ code/exercism.ts
@@ -0,0 +1,17 @@
+//ex 1
+// let pi = '3.14159';
+// let pi = 3.14159;
+// let tau = pi * 2;
+// console.log(`${tau} is ${pi} times two`);
+
+//ex 2
+// let pie: string;
+// pie = 'blueberry'
+// console.log(`I like to eat ${pie}-flavored pie`);
+
+//ex 3
+let isDouglas:boolean = true
+
+console.log(`${isDouglas ? 'oh, hi Douglas!' : 'Who are you?'}`);
+
+
