e74a12ff98d29d4b697366e71b42cea27f58d250
Yone Shinzato <shinzato.yone@capco.com>
2023-01-20 09:51:19 -0300

    objeto com métodos

--- dist/index.js
+++ dist/index.js
@@ -126,8 +126,13 @@ class Users {
         this.name = n;
         this.balance = b;
     }
+    addMoney(amount) {
+        this.balance += amount;
+    }
 }
 const user1 = new Users('Robert', 10);
-const user2 = new Users('Patrick', 33);
+user1.addMoney(100);
+const user2 = new Users('Patrick', 20);
+user2.addMoney(30);
 console.log(user1);
 console.log(user2);
